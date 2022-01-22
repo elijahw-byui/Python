e7d7eaf1d4d3783b9ac71239bfe69ac3328ff8d1
elijahw-byui <elijahw@byui.edu>
2022-01-21 19:51:45 -0700

    basics completed

--- PyProject/test.py
+++ PyProject/test.py
@@ -8,14 +8,9 @@ from pygame.locals import *
 pygame.init()
  
 # Assign FPS a value
-FPS = 30
+FPS = 60
 FramePerSec = pygame.time.Clock()
- 
-# Setting up color objects
-BLUE  = (0, 0, 255)
-RED   = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLACK = (0, 0, 0)
+
 WHITE = (255, 255, 255)
  
 # Setup a 300x300 pixel display with caption
@@ -32,16 +27,36 @@ class Spot(pygame.sprite.Sprite):
         self.image = pygame.image.load("/Users/elijah/Desktop/CSE310/Python Module/PyProject/Assets/Richguy.png")
         self.rect = self.image.get_rect()
         self.isMarble = False
+        self.isValidSpot = True
     def setPosition(self, num1, num2):
         self.rect.center = (num1,num2) 
         self.id = (num1, num2)
+    def getX(self):
+        return self.id[0]
+    def getY(self):
+        return self.id[1]
     def updates(self):
         co1 = self.id[0]
         co2 = self.id[1]
         self.draw(DISPLAYSURF,co1, co2)
     def draw(self, surface,num3,num4):
         self.setPosition(num3, num4)
-        surface.blit(self.image, self.rect) 
+        surface.blit(self.image, self.rect)
+    def validSpot(self, list, num, num2):
+        for item in list: 
+            if item.isMarble and item.id[0] == num and item.id[1] == num2:
+                self.isValidSpot = False
+                return
+            elif item.isMarble and item.id[0] != num and item.id[1] != num2:
+                self.isValidSpot = True
+    def validMove(self, x, y):
+        return
+    def whichDelete(self, x, y):
+        pass
+                
+
+                
+
 
 class Marble(pygame.sprite.Sprite):
     id = (0,0)
@@ -51,6 +66,7 @@ class Marble(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.clicked = False
         self.isMarble = True
+        self.isValidSpot = False
     def setPosition(self, num1, num2):
         self.rect.center = (num1,num2) 
         self.id = (num1, num2)
@@ -62,16 +78,34 @@ class Marble(pygame.sprite.Sprite):
             self.image = pygame.image.load("/Users/elijah/Desktop/CSE310/Python Module/PyProject/Assets/Poorman2.png")
         else:
             self.image = pygame.image.load("/Users/elijah/Desktop/CSE310/Python Module/PyProject/Assets/Poorman.png")
- 
-    def drawMarble(self, num1, num2):
-        pygame.draw.circle(DISPLAYSURF, BLUE, (num1,num2), 30)
-    def moveMarble(self, num3, num4):
-        self.position = pygame.mouse.get_pos
     def draw(self, surface,num3,num4):
         self.setPosition(num3, num4)
         surface.blit(self.image, self.rect) 
-    def resetPosition(self):
-        pass
+    def validSpot(self,list, num, num2):
+        return
+    def validMove(self, spotX, spotY):
+        if self.id[0] == (spotX + 120) and spotY == self.id[1]:
+            return True
+        elif self.id[1] == (spotY + 120) and spotX == self.id[0]:
+            return True
+        elif self.id[0] == (spotX - 120) and spotY == self.id[1]:
+            return True
+        elif self.id[1] == (spotY - 120) and spotX == self.id[0]:
+            return True
+        else:
+            return False
+    def whichDelete(self, spotX, spotY):
+        if self.id[0] == (spotX + 120) and spotY == self.id[1]:
+            return spotX + 60, spotY 
+        elif self.id[1] == (spotY + 120) and spotX == self.id[0]:
+            return spotX, spotY + 60
+        elif self.id[0] == (spotX - 120) and spotY == self.id[1]:
+            return spotX - 60, spotY
+        elif self.id[1] == (spotY - 120) and spotX == self.id[0]:
+            return spotX, spotY - 60
+        
+
+
 
 
 # Creating Marbles and Spots
@@ -109,49 +143,55 @@ createMarblesNew(2,60,360,8)
 createMarblesNew(2,60,300,4)
 createMarblesNew(2,300,300,4)
 
-def isMarbleSelected(list):
-    for item in list:
-        if item.isMarble:
-            if item.clicked:
-                return True
-            else:
-                return False
-
-for item in list:
-    print (item.id)
 DISPLAYSURF.fill(WHITE)
 # Beginning Game Loop
-isClicked= False
+#isClicked = False
+class clickers():
+    def __init__(self):
+        super().__init__()
+        self.isClicked = False
+
+booleo = clickers()
 while True:
     pygame.display.update()
-    #isClicked = False
     for item in list:
         item.updates()
     for event in pygame.event.get():
+        
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-        #if isMarbleSelected(list):
-        if event.type == pygame.MOUSEBUTTONDOWN and not isClicked:
+        if event.type == pygame.MOUSEBUTTONDOWN and not booleo.isClicked:
             for item in list:
                 if item.rect.collidepoint(event.pos):
-                    print(event.pos)
                     if item.isMarble:
                         item.clicked = not item.clicked
-                        isClicked = True
-                        if item.clicked:
-                            print(item.clicked)
-                            #item.setPosition(50,50)
-        elif event.type == pygame.MOUSEBUTTONDOWN and isClicked:
-            for item in list:
-                if item.isMarble and item.clicked:
-                    var = event.pos[0]
-                    var1 = event.pos[1]
-                    item.setPosition(var, var1)
-                    item.clicked = not item.clicked
-                    isClicked = False
+                        booleo.isClicked = not booleo.isClicked
+                        
+        elif event.type == pygame.MOUSEBUTTONDOWN and booleo.isClicked:
+            newLocX = 1 
+            newLocY = 1
+            for item2 in list:
+                if not item2.isMarble and item2.rect.collidepoint(event.pos) and item2.isValidSpot:      
+                    newLocX = item2.getX()
+                    newLocY = item2.getY()
+                    
+                    for item3 in list:
+                        
+                        
+                        if item3.isMarble and item3.clicked and item2.isValidSpot:
+                            if item3.validMove(newLocX,newLocY):
+                                var = item3.whichDelete(newLocX,newLocY)
+                                item3.setPosition(newLocX,newLocY)                                
+                                item3.clicked = not item3.clicked
+                                
+                                booleo.isClicked = not booleo.isClicked
+                                
+                                for items in list:
+                                    if items.id == var and items.isMarble:
+                                        items.setPosition(50,50)
 
-
+                            
 
 
    
